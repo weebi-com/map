9d27deebd370151094d449fc033e037dddc553e9
Birante SY <birantesy@gmail.com>
2024-08-02 09:08:29 +0000

    (feat): Refractor models and map ✨

--- config/routes.rb
+++ config/routes.rb
@@ -3,25 +3,28 @@ Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount ActionCable.server => '/cable'
-  mount MissionControl::Jobs::Engine, at: "/jobs"
+  mount MissionControl::Jobs::Engine, at: '/jobs'
 
   # Defines the root path route ("/")
   root 'map_viewer#index'
 
-  get 'imports/new'
-  get 'imports/create'
+  resources :imports, only: [:new, :create] do
+    collection do
+      post :categories
+    end
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   devise_for :users, path: '/', path_names: { sign_in: 'login', sign_out: 'logout' }, only: [:sessions]
 
-  resources :imports, only: [:new, :create]
+  resources :imports, only: %i[new categories create]
 
   namespace :api do
     namespace :v1 do
-      resources :data_activities, only: [:index]
+      resources :entreprises, only: [:index]
     end
   end
 end
