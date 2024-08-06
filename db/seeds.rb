4c1edad04a470d24f8cf3e567a00ebbc1034d552
Birante SY <birantesy@gmail.com>
2024-08-06 16:57:39 +0000

    Add seed for the first user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create!(email: "hello@weebi.com", password: "hello@weebi.com", password_confirmation: "hello@weebi.com")
